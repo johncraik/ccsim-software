e18da92b231935f9846b6caa00041934f6ebd256
johncraik <jgcraik1@sheffield.ac.uk>
2021-08-20 16:28:43 +0100

    Title

--- scripts/java.js
+++ scripts/java.js
@@ -1,5 +1,11 @@
-//Universal Footer:
+
 window.onload = function(){
+  //Universal Title:
+  var title = "Climate Change Simulation Software | BOO ";
+  var pageID = document.getElementById('pgTitle');
+  document.getElementById('pgTitle') = title + pageID;
+
+  //Universal Footer:
   var relInfo = "Climate Change Simulation Software | Current Release: 1.5.22.2 | Web Version: 2.0.1";
   document.getElementById('relInfo').innerHTML = relInfo;
 }
